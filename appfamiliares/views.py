7df94abddc48c81f237888f137ab7068e5634760
YGH <yasg86@hotmail.com>
2025-08-22 17:06:27 -0300

    "creacion de primer MVT con vistas"

--- appfamiliares/views.py
+++ appfamiliares/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Familiares
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,19 @@ def index(request):
         'mensaje': 'Bienvenido a la sección de Familiares'
     }
     return render(request, 'appfamiliares/index.html',context)
+
+def lista_familiares(request):
+    familiares = Familiares.objects.all()
+    context = {
+        'titulo': 'Listado de Familiares',
+        'familiares': familiares
+    }
+    return render(request, 'appfamiliares/lista_familiares.html', context)
+
+def detalle_familiar(request, id):
+    familiar = Familiares.objects.get(id=id)
+    context = {
+        'titulo': 'Detalle del Familiar',
+        'familiar': familiar
+    }
+    return render(request, 'appfamiliares/detalle_familiar.html', context)
